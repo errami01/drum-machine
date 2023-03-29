b191791b94d206490131d971bb80469c9208bcf9
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-29 11:08:07 +0000

    add isPlaying state

--- src/Pad.js
+++ src/Pad.js
@@ -1,8 +1,12 @@
 import './App.css'
-
+import { useState } from 'react'
 function Pad({id, letter}){
+    const [isPlaying, setIsPlaying] = useState(false)
+    function handleClick(){
+
+    }
     return (
-    <div id={id} className="drum-pad">
+    <div id={id} className="drum-pad" onClick={handleClick}>
       <audio src={`https://s3.amazonaws.com/freecodecamp/drums/${id}.mp3`} ></audio>
       {letter}
     </div>
