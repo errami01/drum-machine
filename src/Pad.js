267ee6afbad4d1770943bb8e6e722f53d50e3305
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2023-03-29 11:31:58 +0000

    Add handleClick and ref to pad components

--- src/Pad.js
+++ src/Pad.js
@@ -1,13 +1,14 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 function Pad({id, letter}){
     const [isPlaying, setIsPlaying] = useState(false)
+    const audio = useRef()
     function handleClick(){
-
+        audio.current.play()
     }
     return (
     <div id={id} className="drum-pad" onClick={handleClick}>
-      <audio src={`https://s3.amazonaws.com/freecodecamp/drums/${id}.mp3`} ></audio>
+      <audio src={`https://s3.amazonaws.com/freecodecamp/drums/${id}.mp3` } ref={audio} ></audio>
       {letter}
     </div>
     )
